3ecaa14f6b6efa26a1f56d97ebd3512343e2ca2d
Unal-223010 <unlklkc@gmail.com>
2022-09-10 13:05:22 +0200

    pushups.sh updated with read -p command

--- bash-scripts/pushups.sh
+++ bash-scripts/pushups.sh
@@ -3,10 +3,11 @@
 
 #while loop will loop or repeat your script over and over . As long as something is true.
 
-x=1
+x=1 # we create a counter for while loop.
 
-while [[ $x -le 100 ]]  # -le means ,  x less than or equal 100.
+while [[ $x -le 10 ]]  # -le means ,  x less than or equal 100.
 do
-    echo "Hey, you just did $x pushups"   # What's going to be looped is between do and done.
+    read -p "Pushup $x : Press enter to continue # -p prompt output the string PROMPT without a trailing newline before attempting to read
     (( x ++ )) # That's means = x + 1 so we increment the variable x by one ,each time it was run .
 done # We must put those to close it out.
+echo " Congrats, you completed your pushups.
