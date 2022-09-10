ddbfba0845a31c58eb58b8b49acedebfac87b96f
Unal-223010 <unlklkc@gmail.com>
2022-09-10 14:00:50 +0200

    comments line added pushups.sh file

--- bash-scripts/pushups.sh
+++ bash-scripts/pushups.sh
@@ -4,9 +4,9 @@
 #while loop will loop or repeat your script over and over . As long as something is true.
 
 #!/bin/bash
-x=1 # we create a counter for while loop.
+x=1 # we create a counter for while loop. Bunun sayesinde infini loop's olmayacak.
 
-while [[ $x -le 10 ]]  # -le means ,  x less than or equal 100.
+while [[ $x -le 10 ]]  # -le means ,  x less than or equal 100. x 10'a esit olunca do-done arasindaki kod'un calismasi bitecektir.
 do
     read -p "Pushup $x : Press enter to continue " # -p prompt output the string PROMPT without a trailing newline before attempting to read
     (( x ++ )) # That's means = x + 1 so we increment the variable x by one ,each time it was run .
