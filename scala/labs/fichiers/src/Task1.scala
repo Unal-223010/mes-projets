46bb6e33a6c26adefe064baeaed8098d166121fe
ted <unlklkc@gmail.com>
2022-12-01 15:30:19 +0100

    lab9 Partie_1 Tache-1 added

--- scala/labs/fichiers/src/Task1.scala
+++ scala/labs/fichiers/src/Task1.scala
@@ -1,3 +1,25 @@
-object Task1 {
+import java.io.FileNotFoundException
+import scala.io.Source
+import scala.io.Source._
+object Task1 extends App {
+  def readFile(filename: String): Array[String] = {
+    //val fileName: String = "test.txt"
+    val lines = Source.fromFile(s"$filename").getLines.toArray
+    for (i <- lines.indices)
+      System.out.println(lines(i))
+    return lines
+  }
 
+  try {
+    //var file_exp1 = readFile("test.pdf")
+    //var file_exp2 = readFile("res/test.pdf")
+    readFile("res/test.txt")
+    readFile("test.pdf")
+
+  }
+  catch {
+    case e : FileNotFoundException =>
+      println("Exception: File missing")
+  }
 }
+
